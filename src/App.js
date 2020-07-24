c49bd5c90d686d56465dcaf4a21e37850426f0db
Carl Padilla <carlpadilla@protonmail.com>
2020-07-24 10:39:50 -0400

    added title and css styling

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@ import React from 'react';
 import './App.css';
 
 //Components
+import Title from './components/title/Title';
 import Quote from './components/quote/Quote';
 import Author from './components/author/Author';
 
 function App() {
   return (
-    <div className='quote-container' id='quote_container'>
-      {/* Quote */}
-      <Quote />
-      {/* Author Currently not used */}
-      <Author />
-      <footer>
-        Made with{' '}
-        <span aria-label='Heart' role='img'>
-          ❤️
-        </span>{' '}
-        by Carl Padilla
-      </footer>
+    <div>
+      <Title />
+      <div className='quote-container' id='quote_container'>
+        {/* Quote */}
+        <Quote />
+        {/* Author Currently not used */}
+        <Author />
+        <footer>
+          Made with{' '}
+          <span aria-label='Heart' role='img'>
+            ❤️
+          </span>{' '}
+          by Carl Padilla
+        </footer>
+      </div>
     </div>
   );
 }
